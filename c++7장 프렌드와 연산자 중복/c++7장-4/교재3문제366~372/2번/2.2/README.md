0f3cf6defe564a1343b3c71a7eecc8fbf1bef64f
2101080JUNGJINYOUNG <spickingerqc@naver.com>
2024-11-04 23:49:47 +0900

    Delete c++7장 프렌드와 연산자 중복/c++7장-4/교재3문제366~372 directory

--- c++7장 프렌드와 연산자 중복/c++7장-4/교재3문제366~372/2번/2.2/README.md
+++
@@ -1,8 +0,0 @@
-#### C++ 7장-4 366~372페이지 문제2.2
-* Book 객체를 활용하는 사례이다.
-![image](https://github.com/user-attachments/assets/f5e1b839-9db2-41b0-8cd2-cbd21929d297)
-* 세 개의 == 연산자를 프렌드 함수로 작성하라.
----
-##### 결과
-![image](https://github.com/user-attachments/assets/14423a9f-e99d-4430-bf3a-7bdfcc09e609)
-
--- c++7장 프렌드와 연산자 중복/c++7장-4/교재3문제366~372/3번/Book.cpp
+++
@@ -1,9 +0,0 @@
-#include "Book.hpp"                                              // 헤더 파일 포함
-
-
-Book::Book(string a,int w,int p):write(a),won(w),page(p){}       // 생성자 정의
-
-bool Book::operator!(){                                          // operator! 함수 정의
-    if(this -> won == 0) return true;                            // 조건문이 참이면 true 반환
-    else return false;                                           // 조건문이 거짓이면 false 반환
-}
